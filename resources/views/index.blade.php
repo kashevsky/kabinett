8f4763c7c01cb9904df53d58cfa0936df94f85d5
kashevsky <kashevsky.d@yandex.ru>
2023-08-24 00:55:14 +0300

    Установка sleepingowl admin

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!doctype html> 
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <title>Всем привет!</title>
+  <meta name="description" content="Я первая страничка">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <h1>Тест</h1>
+</body>
+</html>
